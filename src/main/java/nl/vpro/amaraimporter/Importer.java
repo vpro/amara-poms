bb8d05aa501cde680c5c84da2ae2ffd399151b50
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2016-03-25 11:01:06 +0100

    typos.

--- src/main/java/nl/vpro/amaraimporter/Importer.java
+++ src/main/java/nl/vpro/amaraimporter/Importer.java
@@ -10,7 +10,7 @@ public class Importer {
         MediaRestClient client = new MediaRestClient();
         client.setTrustAll(true);
         client.setUserName("vpro-mediatools");
-        client.setPassword("HAAL WATCHWOORD UIT CONFIG FILE");
+        client.setPassword("HAAL WACHTWOORD UIT CONFIG FILE OF ARGV OF ZOIETS");
         client.setUrl("https://api-test.poms.omroep.nl/");
         client.setThrottleRate(50);
         client.setWaitForRetry(true);
