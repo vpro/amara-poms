cb80348a4244a2119b753e2854d5ac4a4176e76c
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2016-09-21 16:39:54 +0200

    AMARAPOMS-14

--- src/main/java/nl/vpro/amara_poms/poms/fetchers/M4vWithLsFetcher.java
+++ src/main/java/nl/vpro/amara_poms/poms/fetchers/M4vWithLsFetcher.java
@@ -4,6 +4,9 @@ import lombok.ToString;
 import lombok.extern.slf4j.Slf4j;
 
 import java.io.*;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.apache.commons.lang3.StringUtils;
 
@@ -21,10 +24,8 @@ import nl.vpro.util.CommandExecutorImpl;
 public class M4vWithLsFetcher extends AbstractFileCopyFetcher {
 
     CommandExecutor BASH = new CommandExecutorImpl(Config.getRequiredConfig("bash")); // bash does globbing very well!
-
-    File sourceDir = new File(Config.getRequiredConfig("h264.source.dir"));
+    List<File> sourceDirs = Arrays.stream(Config.getRequiredConfigAsArray("h264.source.dir")).map(File::new).collect(Collectors.toList());
     int depth = (int) Config.getRequiredConfigAsLong("h264.source.dir.depth");
-
 
     public M4vWithLsFetcher() {
         super(new File(
@@ -38,32 +39,33 @@ public class M4vWithLsFetcher extends AbstractFileCopyFetcher {
     @Override
     public FetchResult fetch(Program program) {
         String mid = program.getMid();
-        File sourceDir = new File(Config.getRequiredConfig("h264.source.dir"));
-        log.info("Search files in {}", sourceDir);
-        StringWriter files = new StringWriter();
-        String stars = new String(new char[depth]).replaceAll("\0", "*/");
-        File dirs = new File(sourceDir, stars + mid + "/*");
-        BASH.execute(files, "-c", "ls -d -1 " + dirs.toString());
-        if (StringUtils.isNotBlank(files.toString().trim())) {
-            BufferedReader reader = new BufferedReader(new StringReader(files.toString()));
-            String f;
-            try {
-                while ((f = reader.readLine()) != null) {
-                    File candidate = new File(f);
-                    try {
-                        if (candidate.canRead() && candidate.getName().startsWith("sb.") && candidate.getName().endsWith(".m4v")) {
-                            return success(candidate, mid);
+        for (File sourceDir : sourceDirs) {
+            log.info("Search files in {}", sourceDir);
+            StringWriter files = new StringWriter();
+            String stars = new String(new char[depth]).replaceAll("\0", "*/");
+            File dirs = new File(sourceDir, stars + mid + "/*");
+            BASH.execute(files, "-c", "ls -d -1 " + dirs.toString());
+            if (StringUtils.isNotBlank(files.toString().trim())) {
+                BufferedReader reader = new BufferedReader(new StringReader(files.toString()));
+                String f;
+                try {
+                    while ((f = reader.readLine()) != null) {
+                        File candidate = new File(f);
+                        try {
+                            if (candidate.canRead() && candidate.getName().startsWith("sb.") && candidate.getName().endsWith(".m4v")) {
+                                return success(candidate, mid);
+                            }
+                        } catch (IOException e) {
+                            log.error(e.getMessage(), e);
                         }
-                    } catch (IOException e) {
-                        log.error(e.getMessage(), e);
                     }
+                } catch (IOException e) {
+                    log.error(e.getMessage(), e);
                 }
-            } catch (IOException e) {
-                log.error(e.getMessage(), e);
+                log.info("No feasible file found in\n{}", files);
+            } else {
+                log.info("No files found in {}", dirs);
             }
-            log.info("No feasible file found in\n{}", files);
-        } else {
-            log.info("No files found in {}", dirs);
         }
         return FetchResult.notable();
     }
