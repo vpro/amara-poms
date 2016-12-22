9ea6e0b4a6e71f9f6ef4b643218512e314163f52
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2016-12-22 10:37:25 +0100

    AMARAPOMS-20

--- src/main/java/nl/vpro/amara_poms/App.java
+++ src/main/java/nl/vpro/amara_poms/App.java
@@ -1,55 +1,72 @@
 package nl.vpro.amara_poms;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.time.Duration;
+import java.time.Instant;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import nl.vpro.domain.classification.ClassificationServiceLocator;
 import nl.vpro.domain.media.MediaClassificationService;
+import nl.vpro.util.CommandExecutorImpl;
 
 
 /**
  *  Main app
  */
+@Slf4j
 public class App  {
-    private final static Logger LOG = LoggerFactory.getLogger(App.class);
+    
 
     public static void main(String[] args) throws IOException {
 
-        LOG.info("Started...");
+        log.info("Started...");
         ClassificationServiceLocator.setInstance(new MediaClassificationService());
         // check lock file
         Config.init(args);
         Path path = Paths.get(Config.getRequiredConfig("process.lock.filepath"));
         if (Files.exists(path)) {
-            LOG.error("Another AmaraPomsPublisher process is running ({} exists since {}) -> will quit", path, Files.getLastModifiedTime(path).toInstant());
-            System.exit(Config.ERROR_LOCKFILE_EXISTS);
+            Instant lastModified = Files.getLastModifiedTime(path).toInstant();
+            if (Instant.now().minus(Duration.ofHours(4)).isAfter(lastModified)) {
+                CommandExecutorImpl env = new CommandExecutorImpl("/usr/bin/env");
+                long running = env.lines("ps", "u").filter(s -> s.contains("amara_poms_publisher")).count();
+                if (running == 0) {
+                    log.warn("Lock file {} still exists (since {}), but no process found. Will proceed anyway", path, lastModified);
+                    Files.delete(path);
+                }
+
+            } else{
+                log.error("Another AmaraPomsPublisher process is running ({} exists since {}) -> will quit", path, lastModified);
+                System.exit(Config.ERROR_LOCKFILE_EXISTS);
+            }
         }
         int exitCode = -1;
         try {
             Files.createFile(path);
-            LOG.info("Wrote lock file {}", path);
-            LOG.info("=AMARA PUBLISHER (creating new tasks)======================================");
+            log.info("Wrote lock file {}", path);
+            log.info("=AMARA PUBLISHER (creating new tasks)======================================");
             new AmaraPublisher(Config.getFetcher()).processPomsCollection();
-            LOG.info("=POMS PUBLISHER (handling finished tasks)==================================");
+            log.info("=POMS PUBLISHER (handling finished tasks)==================================");
             new PomsPublisher().processAmaraTasks();
             exitCode = 0;
         } catch (Config.Error e) {
-            LOG.error(e.getMessage(), e);
+            log.error(e.getMessage(), e);
             exitCode = e.getErrorCode();
         } catch (Throwable t) {
-            LOG.error(t.getMessage(), t);
+            log.error(t.getMessage(), t);
             exitCode = 1;
         } finally {
-            LOG.info("removing lockfile {}", path);
+            log.info("removing lockfile {}", path);
             Files.delete(path);
             Config.getPomsClient().shutdown();
-            LOG.info("=ready=================================");
-            LOG.info("=======================================");
+            log.info("=ready=================================");
+            log.info("=======================================");
         }
         System.exit(exitCode);
 
