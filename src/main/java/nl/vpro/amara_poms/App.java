e168619c49cf1b7c845f56d87382083d89035f7c
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2017-01-17 11:27:19 +0100

    Cleaned up a bit.

--- src/main/java/nl/vpro/amara_poms/App.java
+++ src/main/java/nl/vpro/amara_poms/App.java
@@ -9,9 +9,6 @@ import java.nio.file.Paths;
 import java.time.Duration;
 import java.time.Instant;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import nl.vpro.domain.classification.ClassificationServiceLocator;
 import nl.vpro.domain.media.MediaClassificationService;
 import nl.vpro.util.CommandExecutorImpl;
@@ -22,36 +19,21 @@ import nl.vpro.util.CommandExecutorImpl;
  */
 @Slf4j
 public class App  {
-    
+
     public static void main(String[] args) throws IOException {
 
         log.info("Started...");
         ClassificationServiceLocator.setInstance(new MediaClassificationService());
-        // check lock file
-        Config.init(args);
-        Path path = Paths.get(Config.getRequiredConfig("process.lock.filepath"));
-        if (Files.exists(path)) {
-            Instant lastModified = Files.getLastModifiedTime(path).toInstant();
-            if (Instant.now().minus(Duration.ofHours(4)).isAfter(lastModified)) {
-                CommandExecutorImpl env = new CommandExecutorImpl("/usr/bin/env");
-                long running = env
-                    .lines("ps", "u")
-                    .filter(line -> line.contains("amara_poms_publisher"))
-                    .count();
-                if (running == 0) {
-                    log.warn("Lock file {} still exists (since {}), but no process found. Will proceed anyway", path, lastModified);
-                    Files.delete(path);
-                }
 
-            } else{
-                log.error("Another AmaraPomsPublisher process is running ({} exists since {}) -> will quit", path, lastModified);
-                System.exit(Config.ERROR_LOCKFILE_EXISTS);
-            }
-        }
-        int exitCode = -1;
+        Config.init(args);
+
+        Path lockFile = Paths.get(Config.getRequiredConfig("process.lock.filepath"));
+        checkLockFile(lockFile);
+
+        int exitCode;
         try {
-            Files.createFile(path);
-            log.info("Wrote lock file {}", path);
+            Files.createFile(lockFile);
+            log.info("Wrote lock file {}", lockFile);
             log.info("=AMARA PUBLISHER (creating new tasks)======================================");
             new AmaraPublisher(Config.getFetcher()).processPomsCollection();
             log.info("=POMS PUBLISHER (handling finished tasks)==================================");
@@ -64,13 +46,35 @@ public class App  {
             log.error(t.getMessage(), t);
             exitCode = 1;
         } finally {
-            log.info("removing lockfile {}", path);
-            Files.delete(path);
+            log.info("removing lockfile {}", lockFile);
+            Files.delete(lockFile);
             Config.getPomsClient().shutdown();
             log.info("=ready=================================");
             log.info("=======================================");
         }
         System.exit(exitCode);
 
+
+    }
+
+    private static void checkLockFile(Path lockFile) throws IOException {
+        if (Files.exists(lockFile)) {
+            Instant lastModified = Files.getLastModifiedTime(lockFile).toInstant();
+            if (Instant.now().minus(Duration.ofHours(4)).isAfter(lastModified)) {
+                CommandExecutorImpl env = new CommandExecutorImpl("/usr/bin/env");
+                long running = env
+                    .lines("ps", "u")
+                    .filter(line -> line.contains("amara_poms_publisher"))
+                    .count();
+                if (running == 0) {
+                    log.warn("Lock file {} still exists (since {}), but no process found. Will proceed anyway", lockFile, lastModified);
+                    Files.delete(lockFile);
+                }
+
+            } else {
+                log.error("Another AmaraPomsPublisher process is running ({} exists since {}) -> will quit", lockFile, lastModified);
+                System.exit(Config.ERROR_LOCKFILE_EXISTS);
+            }
+        }
     }
 }
