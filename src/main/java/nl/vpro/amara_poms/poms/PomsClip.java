8134f0e6c7ef500156158f4e01e9f7a052f68ef8
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2018-07-25 16:50:54 +0200

    Details.

--- src/main/java/nl/vpro/amara_poms/poms/PomsClip.java
+++ src/main/java/nl/vpro/amara_poms/poms/PomsClip.java
@@ -128,6 +128,16 @@ public class PomsClip {
         images.forEach(iu -> iu.setId(null));
         update.setImages(images);
 
+        // Work around misery with invalid images
+        for (ImageUpdate u : images) {
+            if (u.getWidth() != null && u.getWidth() == 0) {
+                u.setWidth(null);
+            }
+            if (u.getHeight() != null && u.getHeight() == 0) {
+                u.setHeight(null);
+            }
+        }
+
         //JAXB.marshal(update, System.out);
         // update
         String newPomsMid = client.set(update);
